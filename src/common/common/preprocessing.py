73bb094336a83e6bd29760a2812c6af842e730f2
Michał Ciesielski <majalcmaj@gmail.com>
2026-09-20 12:36:05 +0200

    extract column type conversion

--- src/common/common/preprocessing.py
+++ src/common/common/preprocessing.py
@@ -1,12 +1,10 @@
-from typing import TYPE_CHECKING
+
+import pandas as pd
 
 from common.consts import CATEGORY_COLUMN, DATE_COLUMN, REQUIRED_COLUMNS, TARGET_COLUMN
 
-if TYPE_CHECKING:
-    import pandas as pd
 
-
-def validate_required_columns_present(sales: "pd.DataFrame") -> None:
+def validate_required_columns_present(sales: pd.DataFrame) -> None:
     missing_columns = REQUIRED_COLUMNS.difference(sales.columns)
     if missing_columns:
         raise ValueError(
@@ -14,7 +12,7 @@ def validate_required_columns_present(sales: "pd.DataFrame") -> None:
         )
 
 
-def columns_to_expected_types(sales: "pd.DataFrame") -> "pd.DataFrame":
+def columns_to_expected_types(sales: pd.DataFrame) -> pd.DataFrame:
     sales[DATE_COLUMN] = pd.to_datetime(sales[DATE_COLUMN], errors="coerce")
     sales[TARGET_COLUMN] = pd.to_numeric(sales[TARGET_COLUMN], errors="coerce")
     sales[CATEGORY_COLUMN] = (
